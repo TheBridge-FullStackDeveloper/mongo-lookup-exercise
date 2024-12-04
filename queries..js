3ec3d17a0c282ff8f9e99f0f8c1efa52ef92c80b
Will-S <willschrick@gmail.com>
2024-12-04 11:01:51 -0800

    done

--- queries..js
+++ queries..js
@@ -8,42 +8,68 @@
   ]);
   
   //Insert 3 top comments (without replies) into the collection comments, associating them with existing users.
+  db.comments.insertMany([
+    {
+      userId: ObjectId("674f5b4b00b0a2faa493a61f"),
+      content: "Este es un comentario.",
+      createdAt: new Date(),
+      parentCommentId: null,
+    },
+    {
+      userId: ObjectId("674f5b4b00b0a2faa493a620"),
+      content: "Otro comentario principal.",
+      createdAt: new Date(),
+      parentCommentId: null,
+    },
+    {
+      userId: ObjectId("674f5b4b00b0a2faa493a621"),
+      content: "Un comentario más.",
+      createdAt: new Date(),
+      parentCommentId: null,
+    },
+  ]);
 
+
+
+//Insert replies for some main comments (threads).
+
+  db.comments.insertMany([
+    {
+      userId: ObjectId("674f5b4b00b0a2faa493a622"),
+      content: "Respuesta al primer comentario.",
+      createdAt: new Date(),
+      parentCommentId: ObjectId("674f5b4b00b0a2faa493a61f"),
+    },
+    {
+      userId: ObjectId("674f5b4b00b0a2faa493a623"),
+      content: "Otra respuesta al primer comentario.",
+      createdAt: new Date(),
+      parentCommentId: ObjectId("674f5b4b00b0a2faa493a61f"),
+    },
+  ]);
   
-  
-  db.comments.aggregate([
+//Make a Consultation with$lookup
+//Type a query to get all comments including:
+//The user who made the comment.
+//If it is a thread, also show the parent comment.
+
+
+
+db.comments.aggregate([
     {
       $lookup: {
         from: "users",
         localField: "userId",
         foreignField: "_id",
-        as: "userDetails",
-      },
+        as: "userDetails"
+      }
     },
     {
       $lookup: {
         from: "comments",
         localField: "parentCommentId",
         foreignField: "_id",
-        as: "parentComment",
-      },
-    },
-  ]);
-  
-
-  
-  db.comments.aggregate([
-    {
-      $lookup: {
-        from: "comments",
-        localField: "_id",
-        foreignField: "parentCommentId",
-        as: "childComment",
-      },
-    },
-    {
-      $match: {
-        childComment: { $ne: [] },
-      },
-    },
+        as: "parentComment"
+      }
+    }
   ]);
